c6fe2d07169c28e9608c727c8dac64f474f94bc6
Vikki <yingjialiu1216@gmail.com>
2025-02-26 17:20:18 -0800

    added play.js

---
+++ play.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {console.log("you ded cuz you idled");})
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
